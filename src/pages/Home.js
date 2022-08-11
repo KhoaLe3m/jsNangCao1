2ffc6c60754a1d2380447a8016c02eb99257884d
KhoaLe3m <95561146+KhoaLe3m@users.noreply.github.com>
2022-08-11 18:39:27 +0700

    feat:Update components & HomePage -Creat components -Update HomePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,41 @@
+import Footer from "../components/footer";
+import Header from "../components/header";
+
 const HomePage = {
     print() {
-        return `<h1>Home Page</h1>`;
+        return /* html */`
+            ${Header.print()}
+            <div class="banner">
+                <img src="">
+            </div>
+            <div class="news">
+                <h2>Tin tức học tập</h2>
+                <div class="grid grid-cols-3 gap-8">
+                    <div class="border p-4">
+                        <a href="">
+                            <img src="https://picsum.photos/300/300">
+                        </a>
+                        <h3><a href="">Tiêu đề bài viết 1</a></h3>
+                        <p>Nội dung bài viết 1</p>
+                    </div>
+                    <div class="border p-4">
+                        <a href="">
+                            <img src="https://picsum.photos/300/300">
+                        </a>
+                        <h3><a href="">Tiêu đề bài viết 1</a></h3>
+                        <p>Nội dung bài viết 1</p>
+                    </div>
+                    <div class="border p-4">
+                        <a href="">
+                            <img src="https://picsum.photos/300/300">
+                        </a>
+                        <h3><a href="">Tiêu đề bài viết 1</a></h3>
+                        <p>Nội dung bài viết 1</p>
+                    </div>
+                </div>
+            </div>
+            ${Footer.print()}
+        `;
     },
 };
 export default HomePage;
