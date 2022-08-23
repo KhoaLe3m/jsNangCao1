07e69341a836b8eb8faaaccdeada0b1b8f46764d
KhoaLe3m <95561146+KhoaLe3m@users.noreply.github.com>
2022-08-23 09:54:08 +0700

    feat: nav

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ const Header = {
                 <div  class="bg-blue-800 py-4 ">
                 <img  src="https://th.bing.com/th/id/OIP.XFXskwxSLJ15Ol7cxkzzWwHaHa?pid=ImgDet&rs=1 "  class="mx-auto w-20">
                 </div>
-                <div class="bg-orange-500">
+                <div class="bg-orange-500" id="main-menu">
                     ${Nav.render()}
                 </div>
             </header>
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,24 +1,34 @@
+import reRender from "../utils/reRender";
+
 const Nav = {
     render() {
         return /* html */ `
-            <ul class="flex  justify-between">
-                <div class="basis-2/3 flex">
+            <nav class="flex  justify-between">
+                <ul class=" flex">
                     <li><a href="/" class="block  py-3 px-4 text-white hover:bg-blue-500">Home Page</a></li>
                     <li><a href="/about"  class="block  py-3 px-4 text-white hover:bg-blue-500">About Page</a></li>
                     <li><a href="/product" class="block  py-3 px-4 text-white hover:bg-blue-500">Product Page</a></li>
-                </div>
-                <div class=" flex ">
-                    <li class="flex items-center" >Hello<span class="block  py-3 px-4 text-white" id="username"></span></li>
-                    <li ><a  class="block  py-3 px-4 text-white hover:bg-blue-500" id="logout">Log out</a></li>
-                </div>
-            </ul>
+                </ul>
+                ${localStorage.getItem("user") ? `<ul class=" flex ">
+                <li class="flex items-center" >Hello<span class="block  py-3 px-4 text-white" id="username"></span></li>
+                <li ><a  class="block  py-3 px-4 text-white hover:bg-blue-500" id="logout">Log out</a></li>
+            </ul>` : ""}
+            </nav>
         `;
     },
     afterRender() {
         const username = document.querySelector("#username");
-
-        const { email } = JSON.parse(localStorage.getItem("user"));
-        username.innerHTML = email;
+        const logout = document.querySelector("#logout");
+        if (username) {
+            const { email } = JSON.parse(localStorage.getItem("user"));
+            username.innerHTML = email;
+        }
+        if (logout) {
+            logout.addEventListener("click", () => {
+                localStorage.removeItem("user");
+                reRender(Nav, "#main-menu");
+            });
+        }
     },
 };
 export default Nav;
